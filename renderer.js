15d9f31b5250b9f2eca8b5360000d730fdfa82c4
Mohammad Raji <moa@dragonfli.es>
2019-03-04 20:26:11 -0500

    initial commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+document.querySelector(".btn-run").addEventListener("click", function(){
+    exec = require('child_process').exec;
+    exec("python -m trace variable " + this.config_filename + " " + filename);
+});
